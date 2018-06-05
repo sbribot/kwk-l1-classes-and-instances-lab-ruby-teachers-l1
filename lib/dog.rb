cc6f3886624981a7e66cbc8577a90fd60e4d33ec
Sara Beth Ribot <sbribot@gmail.com>
2018-06-05 04:06:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+
+class Dog 
+  def initialize(name)
+    @name = name
+  end
+  
+def name
+  @name
+end
+end 
+
+fido = Dog.new("fido")
+lassie = Dog.new("lassie")
+snoopy = Dog.new("snoopy")
+
